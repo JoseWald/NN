8907a535e4707851640900eba6d519b416532c34
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-09-18 11:13:20 +0300

    test 1

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ x_enter=x_enter/np.max(x_enter,axis=0)
 
 x=np.split(x_enter,[8])[0]
 x_prediction=np.split(x_enter,[8])[1]
+
+class Neural_network(object):
+    def __init__(self):
+        self.input_size=2
+        self.output_size=1
+        self.hidden_size=3
+
+        self.w1=np.random.randn(self.input_size,self.hidden_size)
+        self.w2=np.random.randn(self.hidden_size,self.output_size)
+
+    def forward(self,X):
+        self.z=np.dot(X,self.w1)
+        self.z2=self.sigmoid(self.z)
+        self.z3=np.dot(self.z2,self.w2)
+        o=self.sigmoid(self.z3)
+        return o
+    
+    def sigmoid(self,s):
+        return 1/(1 + np.exp(-s))
+    
+NN=Neural_network()
+n=NN.forward(x)
+print("Output predicted by IA"+ str(n))
+print("The right output should by:"+str(y))
